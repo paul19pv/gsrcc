300239b19a7ffa4aa7ab574ed7ffbb068c34fcb7
fstani@gmail.com <fstani@gmail.com@6e187482-b2de-e4bd-779e-3b4c0c0467cf>
2012-09-18 14:38:23 +0000

    Initial commit, first version

---
+++ trunk/GeoServerRest/GeoServer/RestUtil.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeoServerRest.GeoServer
+{
+    public class RestUtil
+    {
+        public static String entryKey(String key, Boolean value) {
+            return "<entry key=\"" + key + "\">" + value + "</entry>";
+        }
+    }
+}
